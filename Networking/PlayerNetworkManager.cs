e94e32d0b0cc723fbf5a17cd0e72b6d82f242fb0
goneyacine <52914216+goneyacine@users.noreply.github.com>
2020-12-24 13:49:34 +0100

    adding basic error detection system

--- Networking/PlayerNetworkManager.cs
+++ Networking/PlayerNetworkManager.cs
@@ -8,6 +8,7 @@ using UnityEngine.SceneManagement;
 public class PlayerNetworkManager : NetworkBehaviour
 { 
    private void Update(){
+    try{
     //if the game doesn't start yet 
     if(gameStarted == false){
       //we check if we can start the game if all the maps are saved
@@ -26,6 +27,11 @@ public class PlayerNetworkManager : NetworkBehaviour
      castleSentFromHostToClient = true;
      mapSaved = true;
     }
+   }
+   }catch(Exception e){
+    oneVoneVarManager.OneVoneVarManager.errorPanel.SetActive(true);
+    oneVoneVarManager.OneVoneVarManager.errorContent.text = e.ToString();
+  
    }
   }
   //here when the second player join the game we try to enable the map editor on player 1 and 2 devices
@@ -57,6 +63,7 @@ public class PlayerNetworkManager : NetworkBehaviour
 }
  //we call this function when one of the two player press space to save the map and continue
   public void SaveCastle(GameObject castleParent){
+    try{
     List<Vector3> positions = new List<Vector3>();
     List<string> names = new List<string>();
     List<float> zRotations = new List<float>();
@@ -70,10 +77,15 @@ public class PlayerNetworkManager : NetworkBehaviour
     }else{
       RpcSendCastleFromHostToClient(names,positions,zRotations);
     }
-   }
+     }catch(Exception e){
+      oneVoneVarManager.OneVoneVarManager.errorPanel.SetActive(true);
+    oneVoneVarManager.OneVoneVarManager.errorContent.text = e.ToString();
+     }
+ }
    //we call this commad on client (onlyClient not a host) to send his map to the host
    [Command]
    void CmdSendCastleFromClientToHost(List<string> names,List<Vector3> positions,List<float> zRotations){
+    try{
    if(!isServer)
     return;
     oneVoneVarManager.OneVoneVarManager.positions = positions;
@@ -81,24 +93,38 @@ public class PlayerNetworkManager : NetworkBehaviour
     oneVoneVarManager.OneVoneVarManager.zRotations = zRotations;
     castleSentFromClientToHost = true;
     Debug.Log("castle sent from client to host");
+    }catch(Exception e){
+    oneVoneVarManager.OneVoneVarManager.errorPanel.SetActive(true);
+    oneVoneVarManager.OneVoneVarManager.errorContent.text = e.ToString();
+    }
    }
    //we call this ClientRpc on the host to his map to client(onlyClient)
    [ClientRpc]
    void RpcSendCastleFromHostToClient(List<string> names,List<Vector3> positions,List<float> zRotations){
+    try{
     if(isServer)
      return;
   oneVoneVarManager.OneVoneVarManager.positions = positions;
   oneVoneVarManager.OneVoneVarManager.names = names;  
   oneVoneVarManager.OneVoneVarManager.zRotations = zRotations;  
    Debug.Log("castle sent from host to client"); 
+   }catch(Exception e){
+    oneVoneVarManager.OneVoneVarManager.errorPanel.SetActive(true);
+    oneVoneVarManager.OneVoneVarManager.errorContent.text = e.ToString();
+   }
    }
    //this clientRpc is to enable the gameplay tools on the client
    [ClientRpc]
    void RpcStartGameOnClient(){
+    try{
     if(isServer)
     return;
      oneVoneVarManager.OneVoneVarManager.mapEditor.SetActive(false);
      oneVoneVarManager.OneVoneVarManager.gameplayWindowObject.SetActive(true);
+     }catch(Exception e){
+      oneVoneVarManager.OneVoneVarManager.errorPanel.SetActive(true);
+    oneVoneVarManager.OneVoneVarManager.errorContent.text = e.ToString();
+     }
    }
    private bool castleSentFromClientToHost = false;
    private bool castleSentFromHostToClient = false;
